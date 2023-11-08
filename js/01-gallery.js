63f1134adcd3e020306253d7d8e14e9221d45726
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2023-11-08 12:41:12 +0200

    add itemgallery

---
+++ js/01-gallery.js
@@ -0,0 +1,33 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+// const instance = basicLightbox.create(`
+//     <h1>Dynamic Content</h1>
+//     <p>You can set the content of the lightbox with JS.</p>
+// `);
+
+// instance.show();
+
+const container = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+
+`
+  )
+  .join("");
+
+container.insertAdjacentHTML("beforeend", markup);
