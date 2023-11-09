d45c6d762034a0c72c94448c04cafd630a85665e
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2023-11-09 21:24:10 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -27,67 +27,34 @@ const markup = galleryItems
 
 container.insertAdjacentHTML("beforeend", markup);
 
-// container.addEventListener("click", onClick);
+container.addEventListener("click", onClick);
 
-// function onClick(evt) {
-//   evt.preventDefault();
-
-//   if (evt.target === evt.currentTarget) {
-//     return;
-//   }
-
-//   const currentProduct = evt.target.closest(".gallery__item");
-
-//   container.forEach((item) => {
-//     const imgOriginal = document.querySelector(".gallery__image");
-//     console.log(`Img ${item.imgOriginal}`);
-
-//     const imgModal = imgOriginal.getAttribute("data-source");
-
-//     const instance = basicLightbox.create(`
-// <div class ="modal">
-// <img src="${imgModal}" width="800" height="600">
-// </div>
-// `);
-
-//     instance.show();
-//   });
-// }
-
-// const container = document.querySelectorAll(".gallery__item");
-
-// container.forEach(item => {
-//   const image = item.querySelector(".gallery__image");
-//   const originalSrc = image.getAttribute("data-source");
-
-//   //   // Запобігаємо переходу за посиланням
-//   item.addEventListener("click", (e) => {
-//     e.preventDefault();
-
-//     //     // Замінюємо значення атрибута src в модальному вікні
-//     const lightbox = basicLightbox.create(`
-//     <img src="${originalSrc}">
-//   `);
-
-//     //     // Відкриваємо модальне вікно
-//     lightbox.show();
-//   });
-// });
-
-container.addEventListener('click', function (event) {
+function onClick(event) {
   event.preventDefault();
 
   if (event.target.classList.contains('gallery__image')) {
     const source = event.target.getAttribute('data-source');
     
-    // Open the modal
+   
     const lightbox = basicLightbox.create(`
       <img src="${source}" alt="Image description">
     `);
 
     lightbox.show();
+  } else if (evt.target === evt.currentTarget) {
+    return;
   }
-});
+};
+
+
+
+//   const currentProduct = evt.target.closest(".gallery__item");
+
+
+
+
+
+
 
 
 
